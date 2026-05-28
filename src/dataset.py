3ec6bdb0b14d25d28f00e81513749aca3e12fe7e
KDA1325 <1325680q@gmail.com>
2026-05-29 00:31:58 +0900

    feat: GPTDataset DataLoader 생성을 구현
    create_dataloader에서 GPTDataset을 생성하고 torch.utils.data.DataLoader로 감싸 반환하도록 구현한다.
    
    GPTDataset의 샘플 개수 계산 조건과 샘플 조회 루프를 정리해, context_length와 stride 기준으로 input/target 구간을 더 명확하게 생성하도록 보완한다.

--- src/dataset.py
+++ src/dataset.py
@@ -38,17 +38,15 @@ class GPTDataset(Dataset):
         # for문으로 범위를 계속 돌면 나중에 start가 리스트 길이를 넘어가도 계속 검사
         # sample 생성 가능할 때까지만 반복하도록 하기
         #for i in range(0, len(token_ids)):
-        while(len(self.token_ids) != 0):
+        while(start + self.context_length < len(self.token_ids)):
             # start + self.context_length에서 target을 만들 수 있으면 input도 만들 수 있다
             # target은 start + 1에서 시작해서 context_length개 -> 마지막 인덱스는 길이 - 1 값!
-            if start + self.context_length < len(token_ids):
-                # sample 생성 가능
-                samples += 1
 
-                # 이후 start 인덱스 
-                start += self.stride
-            else:
-                break
+            # sample 생성 가능
+            samples += 1
+
+            # 이후 start 인덱스 
+            start += self.stride
 
         self._length = samples
 
@@ -68,24 +66,20 @@ class GPTDataset(Dataset):
             input_ids: (context_length,)
             target_ids: (context_length,)
         """
-        # idx번째 학습 샘플을 구하기 위한 start 인덱스 계산 
-        start = idx * self.stride
-    
         # input을 저장할 리스트
         input = []
         
         # target을 저장할 리스트
         target = []
-        
+
+        # idx번째 학습 샘플을 구하기 위한 start 인덱스 계산 
+        start = idx * self.stride
+    
         # start 인덱스부터 시작, context_length 까지 반복
-        for i in range(start, start + self.context_length):
-            # 현재 인덱스에서 샘플 만들 수 있는지 검사
-            if start + self.context_length < len(self.token_ids):
-                # start 인덱스부터 context_length까지 있는 요소들을 하나씩 리스트에 넣어줌 
-                input.append(self.token_ids[i])
-                target.append(self.token_ids[i + 1])
-            else:
-                break
+        for i in range(0, self.context_length):
+            # start 인덱스부터 context_length까지 있는 요소들을 하나씩 리스트에 넣어줌 
+            input.append(self.token_ids[start + i])
+            target.append(self.token_ids[start + i + 1])
         
         # 리스트를 LongTensor로 변환
         input_ids = torch.tensor(input, dtype=torch.long)
@@ -107,4 +101,36 @@ def create_dataloader(
     num_workers: int = 0,
 ) -> DataLoader:
     """TODO: GPTDataset을 만들고 torch.utils.data.DataLoader로 감싸 반환합니다."""
-    raise NotImplementedError("create_dataloader를 구현하세요.")
+
+    # GPTDataset 객체 만들어주기 
+    dataset = GPTDataset(token_ids, context_length, stride)
+
+    # dataloader 인자 순서
+    #     self,
+    #     dataset: Dataset[_T_co],
+    #     batch_size: int | None = 1,
+    #     shuffle: bool | None = None,
+    #     sampler: Sampler | Iterable | None = None,
+    #     batch_sampler: Sampler[list] | Iterable[list] | None = None,
+    #     num_workers: int = 0,
+    #     collate_fn: _collate_fn_t | None = None,
+    #     pin_memory: bool = False,
+    #     drop_last: bool = False,
+    #     timeout: float = 0,
+    #     worker_init_fn: _worker_init_fn_t | None = None,
+    #     multiprocessing_context=None,
+    #     generator=None,
+    #     *,
+    #     prefetch_factor: int | None = None,
+    #     persistent_workers: bool = False,
+    #     pin_memory_device: str = "",
+    #     in_order: bool = True,
+    # 인자가 엄청 많아서 인자 순서를 하나하나 맞추기 힘듦
+    # -> 이름을 명시해서 넣어준다 => batch_size=batch_size
+    
+    # DataLoader 객체 만들어주기 
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, drop_last=drop_last, num_workers=num_workers)
+
+    return dataloader
+
+    # raise NotImplementedError("create_dataloader를 구현하세요.")
