092ed0d76d06478654e83d3c6f461fea39acb2d1
KDA1325 <1325680q@gmail.com>
2026-05-29 13:33:58 +0900

    feat: MultiHeadAttention 초기 레이어 구성을 구현
    MultiHeadAttention.__init__에서 QKV projection, output projection, dropout 레이어를 정의한다.
    
    입력 embedding 차원에서 Query, Key, Value를 한 번에 생성할 수 있도록 3배 출력 차원의 선형 레이어를 추가하고, head 병합 뒤 모델 차원으로 되돌리는 projection과 dropout을 초기화한다.

--- src/attention.py
+++ src/attention.py
@@ -19,7 +19,7 @@ class MultiHeadAttention(nn.Module):
 
     def __init__(
         self,
-        d_model: int,
+        d_model: int, # == emb_dim -> 토큰 하나를 표현하는 벡터 길이
         n_heads: int,
         drop_rate: float = 0.1,
         qkv_bias: bool = False,
@@ -30,8 +30,23 @@ class MultiHeadAttention(nn.Module):
         self.d_model = d_model
         self.n_heads = n_heads
         self.head_dim = d_model // n_heads
-        # TODO: qkv projection, output projection, dropout을 정의하세요.
-        raise NotImplementedError("MultiHeadAttention.__init__을 구현하세요.")
+        self.drop_rate = drop_rate
+
+        # qkv projection, output projection, dropout을 정의하세요.
+        # qkv_projection: Q, K, V를 만들기 위한 선형 레이어
+        # Liner(self.d_model, 3 * d_model): 토큰 하나 당 d_model 길이의 벡터가 들어옴, 
+        # -> 3 * d_model: Q(Query), K(Key), V(Value) 세 개를 한 번에 만들기 위해 출력 자원을 3 * d_model로 설정
+        self.qkv_projection = nn.Linear(self.d_model, 3 * self.d_model)
+
+        # attention 계산이 끝난 뒤 결과를 다시 모델 차원으로 정리하는 선형 레이어 
+        # 입력 d_model, 출력 d_model -> 중간에 head를 나눴다가 마지막에 다시 합침
+        # 합친 후 shape는 다시 d_model이라 출력 d_model 
+        self.output_projection = nn.Linear(self.d_model, self.d_model)
+
+        # attention weight나 최종 projection 결과에 적용할 dropout 레이어
+        self.dropout = nn.Dropout(self.drop_rate)
+
+        #raise NotImplementedError("MultiHeadAttention.__init__을 구현하세요.")
 
     def forward(
         self,
